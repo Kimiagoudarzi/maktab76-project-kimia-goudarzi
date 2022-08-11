9d2a3e2ab80665afd3dc362b4d82ce3f599196d2
Kimiya <102611881+Kimiagoudarzi@users.noreply.github.com>
2022-08-11 12:34:09 +0430

    card1

--- my-project/src/components/homePage/HomeCard/HomeCard.js
+++ my-project/src/components/homePage/HomeCard/HomeCard.js
@@ -2,7 +2,7 @@ import "./HomeCard.css";
 import cosmetic from "../../../images/1card.jpg";
 import cream from "../../../images/2card.jpg";
 import body from "../../../images/3card.jpg";
-import Hair from "../../../images/5card.jpg";
+import Hair from "../../../images/4card.jpg";
 const HomeCard = () => {
   return (
     <>
