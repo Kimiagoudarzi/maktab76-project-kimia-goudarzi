7d2224c612f92777d87fecec4466f0eb9f7b3de1
Kimiya <kimia.goudarzi81@gmail.com>
2022-08-22 11:43:49 +0430

    validation form

--- my-project/src/pages/login/index.js
+++ my-project/src/pages/login/index.js
@@ -1,6 +1,6 @@
-import React from "react";
+import { useEffect, useState } from "react";
 import { MDBContainer, MDBRow, MDBCol, MDBInput } from "mdb-react-ui-kit";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 import pic from "assets/images/p44.png";
 import butterfly from "assets/images/butter.png";
 import "./login.css";
@@ -8,6 +8,47 @@ import "./login.css";
 
 
 const Login = () => {
+  const initialValues = {userName: "", password: ""};
+  const [formValues, setFormValues] = useState(initialValues);
+  const [formErrors, setFormErrors] = useState({});
+  const [isSubmit, setIsSubmit] = useState(false);
+  const navigate = useNavigate();
+
+  const handleChange = (e) =>{
+    const {name, value} = e.target;
+    setFormValues({...formValues,[name]: value});
+    console.log(formValues);
+  }
+
+  const handleSubmit = (e)=>{
+    e.preventDefault();
+    setFormErrors(validate(formValues));
+    setIsSubmit(true);
+  }
+
+  useEffect(()=>{
+    console.log(formErrors);
+    if(Object.keys(formErrors).length === 0 && isSubmit){
+      console.log(formValues)
+    }
+  }, [formErrors]);
+  const validate = (values)=>{
+    const errors = {};
+    if(!values.userName){
+      errors.userName = "نام کاربری الزامی است"
+    }else if(values.userName.length < 3){
+      errors.userName = "نام کاربری باید بیشتر از 3 کاراکتر باشد"
+
+    }
+    else if(!values.password){
+      errors.password = "رمز عبور الزامی است"
+    }else if (values.password.length <4){
+      errors.password = "رمز عبور باید بیشتر از 4 کاراکتر باشد"
+    }else if (values.password.length >10){
+      errors.password = "رمز عبور نباید بیشتر از 10 کارکتر باشد"
+    }
+    return errors;
+  }
 
   return (
     <section className="login-body">
@@ -26,30 +67,37 @@ const Login = () => {
                   <img src={butterfly} style={{ width: "185px" }} alt="logo" />
                   <h4 className="mt-1 mb-5 pb-1">ورود به پنل مدیریت میا لند</h4>
                 </div>
-                <form>
+                {Object.keys(formErrors).length === 0 && isSubmit ? (navigate("/loginForm/admin/products")): (console.log("error"))}
+                <form onSubmit={handleSubmit}>
                 <MDBInput
                   wrapperClass="mb-4"
                   placeholder="نام کاربری"
-                  id="form1"
+                  id="fname"
+                  name="userName"
                   type="text"
-                  required
+                  value={formValues.userName}
+                  onChange = {handleChange}
                 />
+                <p style={{color: "red", fontSize:"13px",marginTop: "5px"}}>{formErrors.userName}</p>
                 <MDBInput
                   wrapperClass="mb-4"
                   placeholder="رمز عبور"
-                  id="form2"
+                  id="pass"
                   type="password"
-                  required
+                  name="password"
+                  value={formValues.password}
+                  onChange = {handleChange}
                 />
+                <p style={{color: "red", fontSize:"13px",marginTop: "5px"}}>{formErrors.password}</p>
                 <div className="text-center pt-1 mb-5 pb-1">
-                  <Link to="/loginForm/admin/products">
+                  {/* <Link to="/loginForm/admin/products"> */}
                     <button
                       className="mb-4 w-100 gradient-custom-2"
                       type="submit"
                     >
                       ورود
                     </button>
-                  </Link>
+                  {/* </Link> */}
                   <a className="text-muted" href="#!">
                     فراموشی رمز عبور!
                   </a>
